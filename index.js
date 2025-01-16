73501974e23df9a4ede9f8aaa5ee66ff0d03838c
Thongqi <141812298+Thongqi@users.noreply.github.com>
2025-01-16 14:12:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import confetti from 'https://cdn.skypack.dev/canvas-confetti';
 
 document.addEventListener('DOMContentLoaded', function(){
   const btn = document.getElementById("btn")
+  console.log(btn)
 btn.addEventListener("click", makeConfetti)
 })
 
