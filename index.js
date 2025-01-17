e0001542f7611dc845ced9651ebf2f51ab70b664
Thongqi <141812298+Thongqi@users.noreply.github.com>
2025-01-17 14:28:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ import confetti from 'https://cdn.skypack.dev/canvas-confetti';
 document.addEventListener('DOMContentLoaded', function(){
   const btn = document.getElementById("btn")
   console.log(btn)
-btn.addEventListener("click", makeConfetti)
+  btn.addEventListener("click", makeConfetti)
+
+  
 })
 
 function makeConfetti(){
     confetti()
+  const vouchers = ['alps', 'azusa']
+  const gift = array[Math.floor(Math.random() * vouchers.length)];
+  console.log(vouchers[gift])
 }
